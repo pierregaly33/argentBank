2b4fd19b5c8e6f3eea129a14b90366a4f120ae3d
pierregaly33 <galy.pierre31@gmail.com>
2024-08-01 14:00:46 +0200

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/img/argentBankLogo.png";
+
+function Navbar() {
+    return (
+        <nav className="main-nav">
+            <NavLink to="/" className="main-nav-logo">
+                <img className="main-nav-logo-image" src={logo} alt="Logo argentBank" />
+            </NavLink>
+            <div>
+                <NavLink to="" className="main-nav-item">
+                    <i class="fa-solid fa-circle-user"></i>
+                    Sign In
+                </NavLink>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
